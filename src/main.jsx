53073c525f42f27e5f4a73f94dd9c2869dc60441
iffat1404 <iffatspatel14@gmail.com>
2025-06-27 12:25:55 +0530

    remove

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,7 @@ import { BrowserRouter } from 'react-router-dom'
 
 
 createRoot(document.getElementById('root')).render(
- <BrowserRouter basename="/vaadvivaad">
+ <BrowserRouter>
       <Theme>
       <App />
 		</Theme>
